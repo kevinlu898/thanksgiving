e2000efee2990932ef46b5a8cd32f421b002f161
Kevin Lu <kevinlu2882@gmail.com>
2025-05-09 18:40:26 -0700

    Initial commit

---
+++ src/client/java/org/thanks/thanksgiving2/client/Thanksgiving2Client.java
@@ -0,0 +1,10 @@
+package org.thanks.thanksgiving2.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Thanksgiving2Client implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/thanks/thanksgiving2/Thanksgiving2.java
@@ -0,0 +1,10 @@
+package org.thanks.thanksgiving2;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Thanksgiving2 implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
